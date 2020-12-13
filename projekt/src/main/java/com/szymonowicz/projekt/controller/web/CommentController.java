1592793009d021c1ec1518431f77c6dcad4155dc
Szymon Szymonowicz <sz.szymonowicz5@gmail.com>
2020-12-14 00:38:26 +0100

    Added edit comment feature (edit comment view, edit comment mapping and edit comment service method)

--- projekt/src/main/java/com/szymonowicz/projekt/controller/web/CommentController.java
+++ projekt/src/main/java/com/szymonowicz/projekt/controller/web/CommentController.java
@@ -6,9 +6,12 @@ import com.szymonowicz.projekt.service.CommentService;
 import com.szymonowicz.projekt.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 public class CommentController {
@@ -40,5 +43,32 @@ public class CommentController {
         postService.addComment(postId, saveComment);
         return "redirect:/";
     }
+
+    @GetMapping("/comment/edit/{id}")
+    public String editPostView(@PathVariable(name = "id") long commentId, Model model){
+        Optional<Comment> commentOptional = commentService.getComment(commentId);
+
+        if(commentOptional.isEmpty())
+            return "redirect:/";
+
+        Comment comment = commentOptional.get();
+
+        Comment editedComment = new Comment();
+        editedComment.setCommentContent(comment.getCommentContent());
+        editedComment.setUsername(comment.getUsername());
+
+        model.addAttribute("comment", comment);
+        model.addAttribute("editedComment", editedComment);
+
+        return "editCommentView";
+    }
+
+    @PostMapping("/comment/edit/{id}")
+    public String editPost(@PathVariable(name = "id") long commentId, Comment updatedComment){
+        commentService.editComment(commentId, updatedComment);
+
+        return "redirect:/";
+    }
+
 
 }
--- projekt/src/main/java/com/szymonowicz/projekt/service/CommentService.java
+++ projekt/src/main/java/com/szymonowicz/projekt/service/CommentService.java
@@ -2,12 +2,14 @@ package com.szymonowicz.projekt.service;
 
 import com.szymonowicz.projekt.model.Comment;
 import com.szymonowicz.projekt.repository.CommentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class CommentService {
     private final CommentRepository commentRepository;
 
@@ -19,11 +21,17 @@ public class CommentService {
         commentRepository.save(comment);
     }
 
+    public Optional<Comment> getComment(long commentId){
+        return commentRepository.findById(commentId);
+    }
+
     public void deleteComment(long commentId){
         Optional<Comment> commentOptional = commentRepository.findById(commentId);
 
-        if(!commentOptional.isPresent())
+        if(!commentOptional.isPresent()) {
+            log.info("Can't delete comment of id = " + commentId + ", because it doesn't exists!");
             return;
+        }
 
         commentRepository.deleteById(commentOptional.get().getId());
     }
@@ -38,5 +46,20 @@ public class CommentService {
         }
 
         return sum;
+    }
+
+    public void editComment(long commentId, Comment updatedComment) {
+        Optional<Comment> commentOptional = commentRepository.findById(commentId);
+
+        if(!commentOptional.isPresent()){
+            log.info("Post with id -> " + commentId + " <- doesn't exist!");
+            return;
+        }
+
+        Comment commentFromDb = commentOptional.get();
+
+        commentFromDb.setCommentContent(updatedComment.getCommentContent());
+
+        commentRepository.save(commentFromDb);
     }
 }
